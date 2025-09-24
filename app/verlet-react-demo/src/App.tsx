5bec46b32999df15283ea688e38fadd59ceae19f
Franklin Hyriol <franklin.razafinandrasana@pulse.mg>
2025-09-24 09:51:18 +0300

    config project

---
+++ app/verlet-react-demo/src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'verlet-react'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App;
---
+++ packages/verlet-react/src/Button/Button.tsx
@@ -0,0 +1,10 @@
+import { type ButtonHTMLAttributes } from "react";
+
+const Button = ({
+  children,
+  ...props
+}: ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return <button {...props}>{children}</button>;
+};
+
+export default Button;
